54f9061dab995793089996eafb8a4fb164448aab
Ahmad Fadillah <45775729+fadil05me@users.noreply.github.com>
2024-11-28 09:11:33 +0700

    Create master.sh

---
+++ master.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Vars
+KUBECFG="/home/$USER/.kube/config"
+
+# Prompt for the master hostname
+read -p "Enter the hostname for this master node: " HOSTNAME
+hostnamectl set-hostname "$HOSTNAME"
+
+# Install K3s on master
+echo "Installing K3s..."
+curl -sfL https://get.k3s.io | sh -
+
+# Create .kube directory for the user
+echo "Configuring Kubernetes for user $USER..."
+mkdir -p "/home/$USER/.kube"
+chown "$USER:$USER" "/home/$USER/.kube"
+chmod 0755 "/home/$USER/.kube"
+
+# Copy k3s.yaml to user's kube config
+sudo cp /etc/rancher/k3s/k3s.yaml "$KUBECFG"
+chown "$USER:$USER" "$KUBECFG"
+chmod 0600 "$KUBECFG"
+
+# Add KUBECONFIG to user's .bashrc
+echo "export KUBECONFIG=$KUBECFG" >> "/home/$USER/.bashrc"
+
+# Add KUBECONFIG to root's .bashrc
+sudo echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> "/root/.bashrc"
+
+echo "K3s master installation complete."
